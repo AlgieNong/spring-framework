4d938c1bc37a209f168a23a43c138745bb2787c7
shanmengyuan <shanmengyuan@youshucorp.cn>
2024-01-15 19:47:15 +0800

    修改Demo

--- mydemo/src/main/java/org/springframework/MainTest.java
+++ mydemo/src/main/java/org/springframework/MainTest.java
@@ -1,6 +1,9 @@
 package org.springframework;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.po.Person;
 
 /**
@@ -9,9 +12,13 @@ import org.springframework.po.Person;
  */
 public class MainTest {
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean.xml");
-		Person person = applicationContext.getBean("Person", Person.class);
-		System.out.println(person.getId());
-		System.out.println(person.getCat().getColor());
+//		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean.xml");
+//		Person person = applicationContext.getBean("Person", Person.class);
+//		System.out.println(person.getId());
+//		System.out.println(person.getCat().getColor());
+
+		BeanFactory bf = new XmlBeanFactory(new ClassPathResource("bean.xml"));
+		Person bean = (Person) bf.getBean("person");
+		System.out.println(bean.getId());
 	}
 }
